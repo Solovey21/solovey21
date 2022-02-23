afa821b220a7b89a17903c748136f6b68e2c2458
Solovey21 <100288428+Solovey21@users.noreply.github.com>
2022-02-23 17:09:17 +0200

    Add files via upload

---
+++ sender.php
@@ -0,0 +1,20 @@
+<?php
+    $name = $_POST['user_name'];
+	$phone = $_POST['user_phone'];
+    
+
+	$to = "denis.koblya@gmail.com"; 
+	$date = date ("d.m.Y"); 
+	$time = date ("h:i");
+	$from = $email;
+	$subject = "Заявка c сайта";
+
+	
+	$msg="
+    Имя: $name /n
+    Телефон: $phone";
+	mail($to, $subject, $msg, "From: $from ");
+
+?>
+
+<p>Привет, форма отправлена</p>
